073e67640e3745faefc90e740f081ffc107bcff0
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-13 16:53:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,6 @@ def introduction(name,language)
 end
 
 def introduction_with_language(name,language)
-  puts "Hi, my name is #{name}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
   
 end
